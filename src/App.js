8ac6fdd7cdbcf11c2a92059d007813cd72072cf2
Victor <vitek.batishev97@gmail.com>
2022-03-24 01:58:47 +0200

    add ability to edit map

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import React, { Component } from 'react'
+import { MapContainer, TileLayer, FeatureGroup } from 'react-leaflet'
+import L from 'leaflet'
+import { EditControl } from 'react-leaflet-draw'
+import { geojsonData } from './Constants'
 
-function App() {
-  return (
-    <div className='App'>
-      <MapContainer center={[51.505, -0.09]} zoom={13}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
-  )
+export default class EditControlExample extends Component {
+  _onEdited = (e) => {
+    this._onChange()
+  }
+
+  _onCreated = (e) => {
+    this._onChange()
+  }
+
+  _onDeleted = (e) => {
+    this._onChange()
+  }
+
+  _onChange = () => {
+    // save data
+  }
+
+  _onFeatureGroupReady = (reactFGref) => {
+    let leafletGeoJSON = new L.GeoJSON(geojsonData)
+    let leafletFG = reactFGref
+
+    leafletGeoJSON.eachLayer((layer) => {
+      leafletFG.addLayer(layer)
+    })
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <MapContainer center={[37.8189, -122.4786]} zoom={13}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          />
+          <FeatureGroup
+            ref={(reactFGref) => {
+              this._onFeatureGroupReady(reactFGref)
+            }}
+          >
+            <EditControl
+              position='topright'
+              onEdited={this._onEdited}
+              onCreated={this._onCreated}
+              onDeleted={this._onDeleted}
+              draw={{ rectangle: true }}
+            />
+          </FeatureGroup>
+        </MapContainer>
+      </div>
+    )
+  }
 }
-
-export default App
