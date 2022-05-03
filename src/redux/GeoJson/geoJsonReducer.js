cc0506f1528b5760410c2e57c623e149ed007b35
Dmitry220 <dmitry19.kravtsov123@gmail.com>
2022-05-03 14:52:42 +0300

    change geoJson reduser

--- src/redux/GeoJson/geoJsonReducer.js
+++ src/redux/GeoJson/geoJsonReducer.js
@@ -8,20 +8,23 @@ let initialState = {
 export const geoJsonReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_GEO_JSON:
-
        if(Array.isArray(action.payload.geoJsonData)){
-          // console.log('max = ', action.payload.geoJsonData.find(item=>Math.max(Date.parse(item.date))).date)
-          // console.log('min = ', action.payload.geoJsonData.sort((a, b) => Date.parse(a.date) - Date.parse(b.date))[0].json_data)
           return {
              ...state,
              storeGeoJsonForPeriod: action.payload.geoJsonData,
              storeGeoJson: action.payload.geoJsonData.sort((a, b) => Date.parse(a.date) - Date.parse(b.date))[0].json_data
-             //storeGeoJson: null
           }
-       }else {
+       }else if(!action.payload.geoJsonData){
           return {
              ...state,
-             storeGeoJson: action.payload.geoJsonData.json_data
+             storeGeoJson: null,
+             storeGeoJsonForPeriod: null
+          }
+       }
+       else {
+          return {
+             ...state,
+             storeGeoJson: action.payload.geoJsonData.json_data,
           }
        }
     default:
